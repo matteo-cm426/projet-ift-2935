f97124e960cb59b6fe53b62304e75bd11e9d355b
matteo-cm426 <113637635+matteo-cm426@users.noreply.github.com>
2023-04-21 19:22:20 -0400

    Add files via upload

---
+++ requeteSQL.sql
@@ -0,0 +1,14 @@
+BEGIN TRANSACTION;
+set search_path to Biblio;
+
+select * from Emprunts;
+
+--- combien de livre fantaisie emprunter par des femmes
+
+--- qui a emprunter le plus de livre ecrit par jk rowling
+
+--- 
+
+--- 
+
+--- nombre de livres emprunter par adherent en moyenne
